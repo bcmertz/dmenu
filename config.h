28aaf1d4915469d700f59b981507a62f6c979402
thistle <bcmertz@email.wm.edu>
2020-12-11 17:56:25 -0500

    opaque

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const char *fonts[] = {
 	"monospace:size=10",
         "NotoColorEmoji:size=14"
 };
-static const unsigned int bgalpha = 0xe0;
+static const unsigned int bgalpha = OPAQUE;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
