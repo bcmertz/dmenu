2b1abebd00abd2623004f9f270201b5862c78898
thistle <bcmertz@email.wm.edu>
2020-12-31 20:00:33 -0500

    s

--- config.h
+++ config.h
@@ -13,9 +13,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 
 static const char *colors[SchemeLast][2] = {
   /*     fg         bg       */
-  [SchemeNorm] = { "#bbbbbb", "#222222" },
-  [SchemeSel] = { "#eeeeee", "#005577" },
-  [SchemeOut] = { "#000000", "#00ffff" },
+  [SchemeNorm] = { "#eeeeee", "#222222" },
+  [SchemeSel] = { "#eeeeee", "#285577" },
+  [SchemeOut] = { "#000000", "#285577" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
